31527085f9f96c06c1b62ab7fdddc8ad5510f457
desmonddai583 <desmonddai583@gmail.com>
2017-11-10 17:09:16 +0800

    adding phoenix and basic data structure

---
+++ firestorm/lib/firestorm_web/router.ex
@@ -0,0 +1,30 @@
+defmodule FirestormWeb.Router do
+  use FirestormWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", FirestormWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    resources "/users", UserController
+    resources "/categories", CategoryController do
+      resources "/threads", ThreadController
+    end
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", FirestormWeb do
+  #   pipe_through :api
+  # end
+end
