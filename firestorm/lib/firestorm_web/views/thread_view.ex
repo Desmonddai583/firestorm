7c56b680379fdb90751e7ef293d839a2970892f9
desmonddai583 <desmonddai583@gmail.com>
2017-11-14 17:39:37 +0800

    add markdown support for post

--- firestorm/lib/firestorm_web/views/thread_view.ex
+++ firestorm/lib/firestorm_web/views/thread_view.ex
@@ -1,3 +1,10 @@
 defmodule FirestormWeb.ThreadView do
   use FirestormWeb, :view
+  alias FirestormWeb.Markdown
+
+  def markdown(body) do
+    body
+    |> Markdown.render
+    |> raw
+  end
 end
--- firestorm/test/firestorm_web/controllers/page_controller_test.exs
+++ firestorm/test/firestorm_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule FirestormWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "New Category"
   end
 end
